bc6f1cea9f748e3e11c81e357ec20ba711716623
abuzar hamza <aubzarhamza@gmail.com>
2016-12-31 18:34:13 -0600

    add form to submit msg

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,20 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('connected to server!');
 });
+
+socket.on('message', function (message) {
+	console.log('New Message');
+	console.log(message.text);
+});
+
+// handle submitting a new message
+var $form = jQuery('#message-form');
+$form.on('submit', function (event) {
+	event.preventDefault();
+
+	socket.emit('message', {
+		text: $form.find('input[name=message]').val()
+	});
+	//
+	$form.find('input[name=message]').val('');
+});
